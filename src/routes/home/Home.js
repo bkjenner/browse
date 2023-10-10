e2027106ef19b4c61fa581e86d326de1e0d07aa0
Wenchen Zhang <wzhang@method1software.com>
2023-10-10 15:48:05 -0600

    revert: remove GraphQL related implementation from Home.js

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -6,22 +6,9 @@ import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
 import { useQuery, gql } from "@apollo/client";
 
-const GET_CLIENTS = gql`
-    query Client {
-        clients {
-            activated
-            clientname
-        }
-    }
-`;
 
 export default function Home() {
     const theme = useTheme();
 
-    const { loading, error, data } = useQuery(GET_CLIENTS);
-
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error : {error.message}</p>;
-
-    return data.clients.map(({ clientname }) => <div>{clientname}</div>);
+    return (<div>test</div>)
 }
